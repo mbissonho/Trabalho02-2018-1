94c650dd4f301626eb42c3b74127711c229e8a0c
mbissonho <mateus_bissonho@live.com>
2018-06-28 08:40:20 -0300

    Configurando e testando o evento dos botões da Activity Principal.

--- app/src/main/java/br/edu/iff/pooa20181/trabalho02_2018_1/MainActivity.java
+++ app/src/main/java/br/edu/iff/pooa20181/trabalho02_2018_1/MainActivity.java
@@ -2,12 +2,37 @@ package br.edu.iff.pooa20181.trabalho02_2018_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnCandidato;
+    private Button btnEleitor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.btnCandidato = findViewById(R.id.btnCandidato);
+        this.btnEleitor = findViewById(R.id.btnEleitor);
+
+        this.btnCandidato.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Msg","Teste");
+            }
+        });
+
+        this.btnEleitor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Msg","Teste");
+            }
+        });
+
+
     }
 }
